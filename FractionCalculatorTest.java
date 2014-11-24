32ca03a725b5c9bf2dc4e745a893a18e56f8ba36
Annabel Jump <annabel.jump@gmail.com>
2014-11-24 01:50:51 +0000

    Test added, currently throws NumberFormatException

---
+++ FractionCalculatorTest.java
@@ -0,0 +1,8 @@
+//Created by Annabel Jump for Programming in Java Coursework 2
+
+public class FractionCalculatorTest {
+    public static void main(String[] args) {
+        FractionCalculator myCalc = new FractionCalculator();
+        myCalc.letsGo();
+    }
+}
